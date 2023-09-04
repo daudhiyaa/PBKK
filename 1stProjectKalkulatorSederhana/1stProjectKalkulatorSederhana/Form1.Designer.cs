bedd567d2dac3538b18b0bef1dca78ca60295629
Daud Dhiya <daud.dhiya31@gmail.com>
2023-09-05 01:20:56 +0700

    feat: 1st assignment

---
+++ 1stProjectKalkulatorSederhana/1stProjectKalkulatorSederhana/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace _1stProjectKalkulatorSederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            menuStrip1 = new MenuStrip();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(150, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(190, 33);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(150, 112);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(190, 33);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(150, 165);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(190, 33);
+            textBox3.TabIndex = 2;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(60, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Nilai 1 :";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(67, 176);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Hasil :";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(60, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Nilai 2 :";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(73, 252);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 68);
+            button1.TabIndex = 6;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(151, 252);
+            button2.Name = "button2";
+            button2.Size = new Size(72, 68);
+            button2.TabIndex = 7;
+            button2.Text = "*";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(73, 326);
+            button3.Name = "button3";
+            button3.Size = new Size(72, 68);
+            button3.TabIndex = 8;
+            button3.Text = "-";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(151, 326);
+            button4.Name = "button4";
+            button4.Size = new Size(72, 68);
+            button4.TabIndex = 9;
+            button4.Text = "/";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(229, 252);
+            button5.Name = "button5";
+            button5.Size = new Size(74, 142);
+            button5.TabIndex = 10;
+            button5.Text = "C";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { exitToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(381, 24);
+            menuStrip1.TabIndex = 11;
+            menuStrip1.Text = "Exit";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(38, 20);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(381, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Kalkulator Sederhana";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ 1stProjectKalkulatorSederhana/1stProjectKalkulatorSederhana/Form1.cs
@@ -0,0 +1,87 @@
+namespace _1stProjectKalkulatorSederhana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil = nilai1 + nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil = nilai1 * nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil = nilai1 - nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil = nilai1 / nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
